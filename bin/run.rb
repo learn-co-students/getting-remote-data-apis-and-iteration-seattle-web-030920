04205497353b4d4ab6665eee6cc107869cd0517a
christine <bumpouce@gmail.com>
2020-03-23 15:12:23 -0700

    Done.

--- bin/run.rb
+++ bin/run.rb
@@ -5,4 +5,5 @@ require_relative "../lib/command_line_interface.rb"
 
 welcome
 character = get_character_from_user
+# binding.pry
 show_character_movies(character)
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,9 @@
 def welcome
-  # puts out a welcome message here!
+  puts "Welcome to the SWAPI command line interface!"
 end
 
 def get_character_from_user
-  puts "please enter a character name"
-  # use gets to capture the user's input. This method should return that input, downcased.
+  puts "Please enter a character name:"
+  input = gets.chomp
+  input
 end
